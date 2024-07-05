94ddd1c8fb97058a5730d96186f9961b4fa05d5c
Afishnet <jakesovaan@gmail.com>
2024-07-05 13:47:10 +1000

    Removed not needed files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      Weather App
+      <p>
+        This project was coded by{" "}
+        <a href="" target="_blank">
+          Aneta Jakesova
+        </a>{" "}
+        and is open-sourced in{" "}
+        <a href="" target="_blank">
+          GitHub.
         </a>
-      </header>
+      </p>
     </div>
   );
 }
